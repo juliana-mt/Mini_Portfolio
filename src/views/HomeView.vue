412c58e03a21109c030d4aeb1808c76a97d68765
Seu Nome <julianamoreno.est@gmail.com>
2025-05-17 19:13:01 -0300

    Adicionei GIFs dos projetos e refiz o HomeView

--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -1,5 +1,5 @@
 <template>
-    <section class="overflow-hidden bg-gray-200 w-full text-center py-32 pb-32 bg-neutral-100">
+    <section class="overflow-hidden w-full text-center py-32 pb-32 bg-neutral-200">
         <img src="/logo-preta.png" alt="Logo" class="mx-auto mb-4 w-28 h-28 object-contain">
         <h1 class="text-7xl font-bold mb-4">Olá, seja bem-vindo(a)!</h1>
         <p class="text-lg max-w-5xl mx-auto">
@@ -7,26 +7,51 @@
             Este portfólio é onde compartilho meus projetos, desenvolvendo propostas que combinam design, 
             funcionalidade e aprendizados da minha jornada como futura desenvolvedora front-end.
         </p>
-    </section class="overflow-hidden">
-    <div class="h-16 bg-gradient-to-t from-white to-gray-200"></div>
-    <div class="h-16 bg-gradient-to-b from-gray-200 to-white"></div>
-    <div class="text-center py-10">
+    </section>
+
+    <div class="h-16 bg-gradient-to-b from-neutral-200 to-white"></div>
+    
+    <div class="text-center">
         <h2 class="text-4xl font-bold mb-2">Projetos em destaque</h2>
         <p class="text-gray-700 mb-4 text-1xl w-30">
-            Aqui estão alguns dos projetos que desenvolvi durante minha trajetória de estudos. 
+            Aqui estão alguns dos projetos que desenvolvi durante minha trajetória de estudos.<br>
             Cada um deles representa um desafio vencido e novas habilidades adquiridas.
         </p>
     </div>
-    <section class="flex flex-col md:flex-row items-center justify-between gap-8 bg-white p-8 rounded-xl shadow-md">
-        <div class="overflow-hidden flex items-center gap-8">
-            <img src="/logo-preta.png" alt="Projeto" class="w-64 rounded-xl shadow-lg" />
+    <section class="flex flex-col md:flex-row items-center justify-between gap-8 bg-white p-8 rounded-xl shadow-md pb-16">
+        <div class="overflow-hidden flex items-center gap-8 pl-16 pr-16">
+            <img src="/Gif-Vitrine.gif" alt="Projeto" class="h-64 w-auto"/>
             <div>
-                <h2 class="text-xl font-semibold mb-2">Projeto 1</h2>
-                <p class="text-gray-600 mb-4">Descrição breve do projeto explicando o que foi feito e qual tecnologia foi usada.</p>
-                <button class="bg-blue-500 text-white px-4 py-2 rounded hover:bg-blue-600">Ver mais</button>
+                <h2 class="text-xl font-semibold mb-2 text-gray-800">Projeto: <strong class="text-black">Vitrine Virtual</strong></h2>
+                <p class="text-gray-600 mb-4"><strong>Protótipo de e-commerce</strong> em HTML e CSS voltado para uma <strong>vitrine virtual de roupas</strong>, com layout responsivo e compatível com diversos dispositivos. 
+                    O site inclui funcionalidades como suporte ao usuário, cadastro de lojas, opção de cadastro de usuários (não obrigatório), além de filtragem de produtos por categorias e estações climáticas. 
+                    Todas as funcionalidades foram pensadas para proporcionar uma experiência intuitiva, clara e agradável ao usuário, com foco na usabilidade e navegação fluida. 
+                    O projeto é colaborativo, elaborado no contexto de uma atividade da faculdade</p>
+                <p class="pb-2">Tecnologias Utilizadas</p>
+                <div class="flex gap-4 items-center pb-2">
+                    <img src="https://cdn.iconscout.com/icon/free/png-256/free-figma-3521426-2944870.png?f=webp" alt="Figma" class="w-8 h-8 rounded-md animate-bounce" />
+                    <img src="https://cdn.worldvectorlogo.com/logos/css-3.svg" alt="CSS" class="w-8 h-8 rounded-md animate-bounce" />
+                    <img src="https://cdn.iconscout.com/icon/free/png-256/free-html-5-1-1175208.png" alt="HTML" class="w-8 h-8 rounded-md animate-bounce" />
+                </div>
+                <a href="https://www.figma.com/design/t3vmL2BwgHsG2a7d8grFEl/Poma-Figma?node-id=0-1&t=2o4iQB4aQvI7xxo6-1" class="bg-rose-400 text-white px-3 py-3 rounded-lg">Ver <strong>Vitrine Virtual</strong></a>            </div>
+        </div>
+    </section>
+    <section class="flex flex-col md:flex-row items-center justify-between gap-8 bg-white p-8 rounded-xl shadow-md pb-16">
+        <div class="overflow-hidden flex items-center gap-8 pl-16 pr-16">
+            <img src="/Gif-Saf.gif" alt="Projeto" class="h-64 w-auto"/>
+            <div>
+                <h2 class="text-xl font-semibold mb-2 text-gray-800">Projeto: <strong class="text-black">Sistema de Acompanhamento de Funcionário (SAF)</strong></h2>
+                <p class="text-gray-600 mb-4"><strong>Sistema de Acompanhamento de Funcionários</strong> é um projeto em desenvolvimento, realizado em grupo como parte de uma atividade acadêmica da faculdade. 
+                    Estamos desenvolvendo uma solução digital que visa facilitar o controle de informações como tempo de casa, promoções, realocações e dados cadastrais dos funcionários. 
+                    O sistema está sendo construído com foco na organização, simplicidade e usabilidade, refletindo os aprendizados adquiridos ao longo do curso.</p>
+                <div class="flex gap-4 items-center pb-2">
+                    <img src="https://cdn.iconscout.com/icon/free/png-256/free-figma-3521426-2944870.png?f=webp" alt="Figma" class="w-8 h-8 rounded-md animate-bounce" />
+                    <img src="https://cdn.worldvectorlogo.com/logos/css-3.svg" alt="CSS" class="w-8 h-8 rounded-md animate-bounce" />
+                    <img src="https://cdn.iconscout.com/icon/free/png-256/free-html-5-1-1175208.png" alt="HTML" class="w-8 h-8 rounded-md animate-bounce" />
+                    <img src="https://bognarjunior.wordpress.com/wp-content/uploads/2018/01/1crcyaithv7aiqh1z93v99q.png?w=256" alt="JS" class="w-8 h-8 rounded-md animate-bounce" />
+                </div>
+                <a href="https://github.com/Flazao/SAF-Principal" class="bg-rose-400 text-white px-3 py-3 rounded-lg">Ver <strong>SAF</strong></a>
             </div>
         </div>
     </section>
-
-
 </template>
