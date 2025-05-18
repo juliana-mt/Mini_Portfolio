9479d98a4b7a63b6acd5c6a77caad43a497b992d
Seu Nome <julianamoreno.est@gmail.com>
2025-05-18 19:36:31 -0300

    Finalizando a o portifólio com a página Contato

--- src/views/ContatoView.vue
+++ src/views/ContatoView.vue
@@ -1,3 +1,29 @@
 <template>
-    <h1>contato</h1>
+    <section class="bg-gray-100 py-12 px-4 sm:px-6 lg:px-8">
+        <h1 class="text-3xl text-left px-16 py-2 text-black"><strong>Entre em contato comigo!</strong></h1>
+        <h1 class="text-xl text-left px-16 py-2">Tire suas duvidas, faça orçamentos e contrate meus serviços por aqui.</h1>
+        <div class="max-w-2xl mx-auto bg-white shadow-xl rounded-2xl p-8">
+            <h2 class="text-xl font-bold mb-6 text-gray-800">Preencha os campos</h2>
+            <form action="#" method="POST" class="space-y-6">
+                <div>
+                    <label for="name" class="block text-sm font-medium text-gray-700">Nome</label>
+                    <input type="text" id="nome" name="nome" required
+                    class="mt-1 block w-full px-4 py-2 border border-gray-300 rounded-lg shadow-sm" />
+                </div>
+                <div>
+                    <label for="email" class="block text-sm font-medium text-gray-700">Email</label>
+                    <input type="email" id="email" name="email" required
+                    class="mt-1 block w-full px-4 py-2 border border-gray-300 rounded-lg shadow-sm" />
+                </div>
+                <div>
+                    <label for="message" class="block text-sm font-medium text-gray-700">Mensagem</label>
+                    <textarea id="messagem" name="messagem" rows="5" required
+                    class="mt-1 block w-full px-4 py-2 border border-gray-300 rounded-lg shadow-sm"></textarea>
+                </div>
+                <div>
+                    <a class="w-full bg-rose-600 text-white py-2 px-4 rounded-lg hover:bg-rose-800 transition duration-200 cursor-pointer">Enviar Mensagem</a>
+                </div>
+            </form>
+        </div>
+    </section>
 </template>
--- src/views/SobreView.vue
+++ src/views/SobreView.vue
@@ -1,3 +1,47 @@
 <template>
-    <h1>sobre</h1>
+    <h1 class="text-3xl text-left px-16 py-8"><strong>Juliana Moreno Torres</strong></h1>
+    <div class="pb-4">
+        <h4 class="text-left px-16"><strong>Idade: </strong>19 anos.</h4>
+        <h4 class="text-left px-16"><strong>Data de nascimento: </strong>31/03/2006</h4>
+        <h4 class="text-left px-16"><strong>Localização: </strong>Pompeia-SP.</h4>
+        <h4 class="text-left px-16"><strong>Objetivo profissional: </strong>Front-end focada em experiência do usuário.</h4>
+    </div>
+    <h2 class="text-2xl text-left px-16"><strong>Minhas Habilidades Técnicas</strong></h2>
+    <div class="flex gap-4 flex-wrap items-center px-16 pt-4 pb-8">
+        <div class="flex flex-col items-center gap-1">
+            <img src="https://cdn.iconscout.com/icon/free/png-256/free-html-5-1-1175208.png" alt="HTML" class="w-16 h-16 animate-pulse" />
+            <span class="text-sm text-orange-700"><strong>HTML</strong></span>
+        </div>
+        <div class="flex flex-col items-center gap-1">
+            <img src="https://cdn.worldvectorlogo.com/logos/css-3.svg" alt="CSS" class="w-16 h-16 animate-pulse" />
+            <span class="text-sm text-sky-700"><strong>CSS</strong></span>
+        </div>
+        <div class="flex flex-col items-center gap-1">
+            <img src="https://bognarjunior.wordpress.com/wp-content/uploads/2018/01/1crcyaithv7aiqh1z93v99q.png?w=256" alt="js" class="w-16 h-16 animate-pulse" />
+            <span class="text-sm text-yellow-500"><strong>JavaScript</strong></span>
+        </div>
+        <div class="flex flex-col items-center gap-1">
+            <img src="https://upload.wikimedia.org/wikipedia/commons/thumb/9/95/Vue.js_Logo_2.svg/1200px-Vue.js_Logo_2.svg.png" alt="Vue" class="w-16 h-16 animate-pulse" />
+            <span class="text-sm text-green-500"><strong>Vue</strong></span>
+        </div>
+        <div class="flex flex-col items-center gap-1">
+            <img src="https://upload.wikimedia.org/wikipedia/commons/thumb/0/0a/Python.svg/640px-Python.svg.png" alt="Python" class="w-16 h-16 animate-pulse" />
+            <span class="text-sm text-yellow-600"><strong>Python</strong></span>
+        </div>
+        <div class="flex flex-col items-center gap-1">
+            <img src="https://pt.vite.dev/logo.svg" alt="Vite" class="w-16 h-16 animate-pulse" />
+            <span class="text-sm text-violet-400"><strong>Vite</strong></span>
+        </div>
+        <div class="flex flex-col items-center gap-1">
+            <img src="https://cdn.iconscout.com/icon/free/png-256/free-figma-3521426-2944870.png?f=webp" alt="Figma" class="w-16 h-16 animate-pulse" />
+            <span class="text-sm text-gray-600"><strong>Figma</strong></span>
+        </div>
+        <div class="flex flex-col items-center gap-1">
+            <img src="https://finevpn.org/wp-content/uploads/2024/02/github-logo-logo.png" alt="GitHub" class="w-16 h-16 animate-pulse" />
+            <span class="text-sm text-black"><strong>GitHub</strong></span>
+        </div>
+    </div>
+    <div class="px-16 pb-8">
+        <button onclick="window.print()" class="px-4 py-2 bg-pink-600 text-white font-semibold rounded-md hover:bg-pink-700 hover:-translate-y-1 transition-transform duration-200">Imprimir pdf</button>
+    </div>
 </template>
